bf96b05cf61e186655836989915c7567075f8484
Hayden Glenn <haydenn.glenn.9@gmail.com>
2026-02-12 00:29:39 -0800

    Fix public layout to isolate dashboard

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,7 @@
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
