0c8f46d4379db19eadbf91320c4a90c142c5cd65
Hayden Glenn <haydenn.glenn.9@gmail.com>
2026-02-13 19:49:14 -0800

    fix profile layout override

--- app/(app)/profile/layout.tsx
+++ app/(app)/profile/layout.tsx
@@ -1,3 +1,9 @@
-export default function ProfileLayout({ children }: { children: React.ReactNode }) {
+// ❗ DO NOT add "use client" here
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <>{children}</>;
 }
