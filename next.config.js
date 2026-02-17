06d1848e8c89fb0969bb5402812a0b9518cf43e6
Hayden Glenn <haydenn.glenn.9@gmail.com>
2026-02-17 11:56:33 -0800

    Allow Wix iframe embedding

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
     serverActions: {
       allowedOrigins: ["*"]
     }
+  },
+  async headers() {
+    return [
+      {
+        source: '/embed/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.wixsite.com https://*.editorx.io https://*.wix.com https://www.queersandalliesfitness.com"
+          }
+        ]
+      }
+    ]
   }
 };
 
